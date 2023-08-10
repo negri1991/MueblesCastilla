a84b6f98e12328c5288e3cbeb2e9c43d485f0c40
negri1991 <el_pecas91@hotmail.com>
2023-08-10 14:49:54 +0200

    Crear interfaz de oducto de usuario

--- src/main/java/es/mueblesCastilla/controller/HomeController.java
+++ src/main/java/es/mueblesCastilla/controller/HomeController.java
@@ -1,16 +1,25 @@
 package es.mueblesCastilla.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.mueblesCastilla.model.Producto;
 import es.mueblesCastilla.service.ProductoService;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService; 
@@ -20,6 +29,17 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id enviado como parámetro {}", id);
+		Producto producto= new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		return "usuario/productohome";
 	}
 	
 
