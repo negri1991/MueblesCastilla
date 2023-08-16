1340fd82abf4af949bd22a05dbcea84d837ddfde
negri1991 <el_pecas91@hotmail.com>
2023-08-16 23:50:54 +0200

    Vista de los detalles de la compra y su funcionalidad

--- src/main/java/es/mueblesCastilla/controller/AdministradorController.java
+++ src/main/java/es/mueblesCastilla/controller/AdministradorController.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.mueblesCastilla.model.Compra;
 import es.mueblesCastilla.model.Producto;
+import es.mueblesCastilla.model.Usuario;
 import es.mueblesCastilla.service.ICompraService;
 import es.mueblesCastilla.service.IDetalleCompraService;
 import es.mueblesCastilla.service.IProductoService;
@@ -45,10 +48,21 @@ public class AdministradorController {
 	}
 	
 	@GetMapping("compras")
-	public String detallesCompra(Model model) {
+	public String compra(Model model) {
 	
 		model.addAttribute("Compras", compras.findAll());
 		return "administrador/compras";
 	}
-
+	
+	@GetMapping("detallescompra/{id}")
+	public String detallesCompra(Model model, @PathVariable Integer id,Usuario usuario) {
+		Compra compra = compras.findById(id).get();
+		model.addAttribute("detalles", compra.getDetalle());
+		//Nombre cliente
+		
+		Usuario nombre = compra.getUsuario();
+		model.addAttribute("nombreCliente", nombre);
+		
+		return"administrador/detallescompra";
+	}
 }
